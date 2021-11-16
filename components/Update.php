e80b18bfbfa5ea5b3aebdba191d7ac169c10f276
CreepWannabe <contact@creep.cat>
2021-11-16 16:52:50 +0100

    completed Update component rearranged components include index

--- components/Update.php
+++ components/Update.php
@@ -1,9 +1,21 @@
 <?php
 Class Update extends Connect {
-    public function __construct() {
+    private $table;
+    private $column;
+    private $value;
+    private $oldValue;
+
+    public function __construct(String $table, String $column, String $oldValue, String $value) {
         $this->executeConnection();
+        $this->table = $table;
+        $this->column = $column;
+        $this->value = $value;
+        $this->oldValue = $oldValue;
     }
 
     public function executeUpdate() {
+        $query = "UPDATE {$this->table} SET {$this->column} = '{$this->value}' WHERE {$this->table}.{$this->column} = '{$this->oldValue}'";
+        $result = $this->connection->query($query);
+        return $result;
     }
 }
--- components/_index.php
+++ components/_index.php
@@ -1,4 +1,5 @@
 <?php
 include_once './components/Connect.php';
-include_once './components/Read.php';
 include_once './components/Create.php';
+include_once './components/Read.php';
+include_once './components/Update.php';
